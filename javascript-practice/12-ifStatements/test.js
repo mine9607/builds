928ea250d834100927eb6795fb73f533b0bb2840
mine9607 <81383389+mine9607@users.noreply.github.com>
2023-06-03 19:50:15 -0500

    added several lessons

---
+++ javascript-practice/12-ifStatements/test.js
@@ -0,0 +1,25 @@
+var youLikeMeat = false;
+var myAge = 20;
+
+if (myAge >= 40) {
+  document.write("You are old");
+} else if (myAge <= 20) {
+  document.write("You are young");
+} else {
+  document.write("You are kinda old");
+}
+
+if (youLikeMeat) {
+  document.write("Here is the meaty menu...");
+} else {
+  document.write("Here is the leafy menu...");
+}
+
+/* var youLikeMeat = false;
+
+if (youLikeMeat) {
+  document.write("Here is the meaty menu...");
+} else {
+  document.write("Here is the leafy menu...");
+}
+ */
