8c94d6470c009325fec5c2bc4964a4fbde3b6411
mine9607 <81383389+mine9607@users.noreply.github.com>
2023-06-11 15:17:55 -0500

    Added event folder

--- javascript-practice/21-objects/test.js
+++ javascript-practice/21-objects/test.js
@@ -34,3 +34,37 @@ console.log(myCar);
 myCar.drive(30, 3);
 myCar.logDriver();
 angieCar.logDriver();
+
+//The Date object
+
+var birthday = new Date(1985, 10, 5, 11, 15, 25);
+var birthday2 = new Date(1985, 10, 5, 11, 15, 25);
+//get the month of the date (0 - 11)
+console.log(birthday.getMonth());
+
+//get the full year (YYYY)
+console.log(birthday.getFullYear());
+
+//get the date of the month (1 - 31)
+console.log(birthday.getMonth());
+
+//get the day of the week (0 - 6)
+console.log(birthday.getDay());
+
+//get the hour of the date (0 - 23)
+console.log(birthday.getHours());
+
+//get the number of milliseconds since the 1st Jan 1970
+console.log(birthday.getTime());
+
+if (birthday == birthday2) {
+  console.log("birthdays are equal");
+} else {
+  console.log("birthdays are not equal");
+}
+//use get Time to compare dates to check equality
+if (birthday.getTime() == birthday2.getTime()) {
+  console.log("birthdays are equal");
+} else {
+  console.log("birthdays are not equal");
+}
