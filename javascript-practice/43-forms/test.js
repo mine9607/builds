0786154e60b43ed5041ff0c69d14237ad89dd297
mine9607 <81383389+mine9607@users.noreply.github.com>
2023-06-12 14:31:35 -0500

    added form validation

--- javascript-practice/43-forms/test.js
+++ javascript-practice/43-forms/test.js
@@ -1,28 +1,12 @@
+var myForm = document.forms.myForm;
 var myMessage = document.getElementById("message");
 
-function showMessage() {
-  myMessage.className = "show";
-}
-
-setTimeout(showMessage, 3000);
-
-//note - use setInterval if you want to create a timed slider carousel in Javascript
-
-var colorChange = document.getElementById("color-changer");
-var colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
-var counter = 0;
-
-function changeColor() {
-  if (counter >= colors.length) {
-    counter = 0;
+myForm.onsubmit = function () {
+  if (myForm.name.value == "") {
+    message.innerHTML = "please enter a name";
+    return false;
+  } else {
+    message.innerHTML = "";
+    return true;
   }
-  colorChange.style.background = colors[counter];
-  counter++;
-}
-
-var myTimer = setInterval(changeColor, 1500);
-
-colorChange.onclick = function () {
-  clearInterval(myTimer);
-  colorChange.innerHTML = "Timer Stopped";
 };
