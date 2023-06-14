b6ee1e9a73696a92a87860da4c2e0e57fceac09e
mine9607 <81383389+mine9607@users.noreply.github.com>
2023-06-14 07:03:50 -0500

    added event properties, inline css and hooks

--- react-practice/dojo-blog/src/Home.js
+++ react-practice/dojo-blog/src/Home.js
@@ -1,19 +1,47 @@
+import { useState } from "react";
+
 const Home = () => {
+  //note - the variable name is not reactive - it will update with the changeName event but not render the new name
+  //note - to make something reactive in React we use a hook: "useState"
+  //note - all hooks in React start with 'use'
+
+  //let name = "mario";
+  //the setName function is what is used to update the state of the variable 'name'
+  const [name, setName] = useState("mario");
+  const [age, setAge] = useState("37");
+
+  //note - by default functiions are given access to event properties 'e' that allow additional functionality
+  //onClick log message to the console and list event properties 'e'
   const handleClick = (e) => {
     console.log("hello world", e);
   };
 
-  const handleClickAgain = (name, e) => {
+  //onClick change the name displayed on the webpage and log the message and its target to console
+
+  /*   const changeName = (name, e) => {
+    name = "luigi";
     console.log(`hello ${name}`, e.target);
+  }; */
+
+  const changeName = () => {
+    setName("luigi");
+    setAge("35");
   };
 
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <button style={{ borderRadius: "15px" }} onClick={handleClick}>
+      <br />
+      <p>
+        {name} is {age}
+      </p>
+      <br />
+      <button style={{ backgroundColor: "slategrey", color: "white", borderRadius: "15px" }} onClick={handleClick}>
         Click Me
       </button>
-      <button onClick={(e) => handleClickAgain("mario", e)}>Click me again</button>
+      <br />
+      <br />
+      <button onClick={(e) => changeName("mario", e)}>Change Name</button>
     </div>
   );
 };
